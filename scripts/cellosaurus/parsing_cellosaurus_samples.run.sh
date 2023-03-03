bf1176ac44e8b895189f61a1e8fc136dbdb7af81
joreynajr <joreyna@live.com>
2023-03-03 15:39:11 -0800

    Full run of parsing_cellosaurus with hichip_db

--- scripts/cellosaurus/parsing_cellosaurus_samples.run.sh
+++ scripts/cellosaurus/parsing_cellosaurus_samples.run.sh
@@ -1,6 +1,6 @@
 # define command line arguments
-input_fn="results/cellosaurus/accessions.txt"
-output_fn="results/cellosaurus/cellosaurus_info.tsv"
+input_fn="results/tracker/cellosaurus_ids.uniq.txt"
+output_fn="results/cellosaurus/cellosaurus_metadata.tsv"
 cs_dir="results/cellosaurus/"
 
 scripts/cellosaurus/parsing_cellosaurus_samples.sh \
