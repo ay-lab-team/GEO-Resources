9f8e348571395586cd1bc180ef8cdd9b1f239c50
joreynajr <joreyna@live.com>
2023-10-23 10:53:16 -0700

    Merging the results from querying SRA/GEO

---
+++ scripts/geo_queries/merge_sra_queries.sh
@@ -0,0 +1,8 @@
+# extract the header
+first_fn=$(ls results/sra/*.meta.major_columns.renamed.tsv | head -1)
+combined_fn="results/sra/combined.meta.major_columns.renamed.tsv"
+sed -n '1p' $first_fn > $combined_fn
+
+# concat the rest of the files
+#find results/sra/ -name "*.meta.major_columns.renamed.tsv" ! -name "combined*" -exec echo {} \;
+find results/sra/ -name "*.meta.major_columns.renamed.tsv" ! -name "combined*" -exec sed '1d' {} >> $combined_fn \;
